45f1de30fe18b955146bcf518fb7e2b363461be3
web1991t <web1991t@gmail.com>
2017-06-15 15:34:39 +0300

    configuring

---
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.emusicstore.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by The Revenant on 6/15/2017.
+ */
+
+@Controller
+
+
+public class HomeController {
+}
