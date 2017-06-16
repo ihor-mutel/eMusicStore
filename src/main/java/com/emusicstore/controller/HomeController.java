806909b129f0917cd9777eb5e0ed31e4300f20a3
web1991t <web1991t@gmail.com>
2017-06-16 12:16:38 +0300

    configure HomeController.java

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.emusicstore.controller;
 
+import com.emusicstore.dao.ProductDao;
+import com.emusicstore.model.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by The Revenant on 6/15/2017.
@@ -10,9 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class HomeController {
+    //we define productDao instance
+    private ProductDao productDao = new ProductDao();
+
+
     @RequestMapping("/")
     public String home(){
         return "home";
     }
 
+    //we created another request of mapping
+    @RequestMapping("/productList")
+    // we defined a function which imported model parameter
+    public String getProducts(Model model) {
+        List<Product> productList = productDao.getProductList();
+        //return first product from the list
+        Product product = productList.get(0);
+        model.addAttribute(product);
+
+        return "ProductList";
+    }
 }
+
