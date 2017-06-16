9cd46a8358ddd6982009fc807b972cb4e1a150aa
web1991t <web1991t@gmail.com>
2017-06-16 09:31:59 +0300

    create getters and setters

--- src/main/java/com/emusicstore/model/Product.java
+++ src/main/java/com/emusicstore/model/Product.java
@@ -13,5 +13,67 @@ public class Product {
     private int unitInStock;
     private String pruductManufacturer;
 
+    public String getProductName() {
+        return productName;
+    }
 
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductCondition() {
+        return productCondition;
+    }
+
+    public void setProductCondition(String productCondition) {
+        this.productCondition = productCondition;
+    }
+
+    public String getProductStatus() {
+        return ProductStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        ProductStatus = productStatus;
+    }
+
+    public int getUnitInStock() {
+        return unitInStock;
+    }
+
+    public void setUnitInStock(int unitInStock) {
+        this.unitInStock = unitInStock;
+    }
+
+    public String getPruductManufacturer() {
+        return pruductManufacturer;
+    }
+
+    public void setPruductManufacturer(String pruductManufacturer) {
+        this.pruductManufacturer = pruductManufacturer;
+    }
 }
