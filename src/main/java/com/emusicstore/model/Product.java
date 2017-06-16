22582f7fb7bce99daa9d6e2801ab0dea3c0c7a47
web1991t <web1991t@gmail.com>
2017-06-16 09:28:24 +0300

    add attributes of the product

--- src/main/java/com/emusicstore/model/Product.java
+++ src/main/java/com/emusicstore/model/Product.java
@@ -4,4 +4,14 @@ package com.emusicstore.model;
  * Created by The Revenant on 6/16/2017.
  */
 public class Product {
+    private String productName;
+    private String productCategory;
+    private String productDescription;
+    private double productPrice;
+    private String productCondition;
+    private String ProductStatus;
+    private int unitInStock;
+    private String pruductManufacturer;
+
+
 }
