895ac306a4dbbf5dd587ac9eb36decab56b3de39
web1991t <web1991t@gmail.com>
2017-06-19 11:14:01 +0300

    link viewProduct.jsp to controller

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -37,5 +37,9 @@ public class HomeController {
 
         return "productList";
     }
+    @RequestMapping("/productList/viewProduct")
+    public String viewProduct(){
+        return "viewProduct";
+    }
 }
 
